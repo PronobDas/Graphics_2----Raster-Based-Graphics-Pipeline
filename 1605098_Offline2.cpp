20afcf8950b9acb638637a7201f6e64f62dfe14a
PronobDas <pronobdas2k16@gmail.com>
2021-06-05 20:49:57 +0600

    stage4 done

--- 1605098_Offline2.cpp
+++ 1605098_Offline2.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <cmath>
 #include <iomanip>
+#include "bitmap_image.hpp"
 using namespace std;
 
 #define PI 2*acos(0.0)
@@ -20,6 +21,12 @@ public:
         z = c;
         w = d;
     }
+
+    double distance(Point p)
+    {
+        return sqrt(pow((p.x-x),2) + pow((p.y-y),2) + pow((p.z-z),2) );
+    }
+
     Point operator+( Point p)
     {
         Point temp = Point();
@@ -43,6 +50,7 @@ public:
         cout << x << "  " << y << "  " << z  << "  "<< w << endl;
     }
 };
+
 
 class Vector
 {
@@ -114,19 +122,6 @@ public:
     }
 };
 
-
-Vector rodrigues( Vector x, Vector a, double ang)
-{
-    double angle = ang*PI/180;
-    double temp1 = Vector::dotMul(a, x);
-    Vector temp2 = Vector::crossMul(a, x);
-
-    double sTheta = sin(angle);
-    double cTheta = sqrt( 1 - sTheta*sTheta );
-
-    Vector temp = x*cTheta + (a*(1-cTheta))*temp1 + temp2*sTheta;
-    return temp;
-}
 
 class Matrix
 {
@@ -239,6 +234,60 @@ Point transformPoint( Matrix mat, Point p)
     return p_new;
 }
 
+
+Vector rodrigues( Vector x, Vector a, double ang)
+{
+    double angle = ang*PI/180;
+    double temp1 = Vector::dotMul(a, x);
+    Vector temp2 = Vector::crossMul(a, x);
+
+    double sTheta = sin(angle);
+    double cTheta = sqrt( 1 - sTheta*sTheta );
+
+    Vector temp = x*cTheta + (a*(1-cTheta))*temp1 + temp2*sTheta;
+    return temp;
+}
+
+
+class Color
+{
+public:
+    double R, G, B;
+
+    Color(){}
+
+    Color(double r, double g, double b)
+    {
+        R = r;
+        G = g;
+        B = b;
+    }
+
+    void print()
+    {
+        cout << R << " " << G << " " << B << endl;
+    }
+};
+
+
+class Triangle
+{
+public:
+    Point points[3];
+    Color color;
+
+    Triangle(){}
+
+    Triangle(Point p1, Point p2, Point p3, Color c)
+    {
+        points[0] = p1;
+        points[1] = p2;
+        points[2] = p3;
+        color = c;
+    }
+};
+
+
 int main(void)
 {
     stack<Matrix> sm;
@@ -256,8 +305,7 @@ int main(void)
     ofstream stage1;
     scene.open("scene.txt");
     stage1.open("stage1.txt");
-    stage1 << std::fixed;
-    stage1 << std::setprecision(7);
+    stage1 << std::fixed << std::setprecision(7);
 
     scene >> eyeX >> eyeY >> eyeZ;
     scene >> lookX >> lookY >> lookZ;
@@ -274,7 +322,7 @@ int main(void)
         if (cmd == "triangle")
         {
             tCount++;
-            cout << "triangle" << endl;
+            //cout << "triangle" << endl;
             for (int i = 0; i < 3; i++)
             {
                 double d1, d2, d3;
@@ -282,7 +330,7 @@ int main(void)
 
                 Point p(d1, d2, d3, 1);
                 Point temp = transformPoint( currentTop, p );
-                temp.print();
+                //temp.print();
 
                 stage1 << temp.x << "  " << temp.y << "  " << temp.z << endl;
             }
@@ -290,7 +338,7 @@ int main(void)
         }
         else if (cmd == "translate")
         {
-            cout << "translate"<< endl;
+            //cout << "translate"<< endl;
             double tx, ty, tz;
             scene >> tx >> ty >> tz;
             Matrix temp = Matrix::identityMatrix(4);
@@ -304,7 +352,7 @@ int main(void)
 
         else if (cmd == "scale")
         {
-            cout << "scale"<< endl;
+            //cout << "scale"<< endl;
             double sx, sy, sz;
             scene >> sx >> sy >> sz;
             Matrix temp = Matrix::identityMatrix(4);
@@ -317,7 +365,7 @@ int main(void)
 
         else if (cmd == "rotate")
         {
-            cout << "rotate"<< endl;
+            //cout << "rotate"<< endl;
 
             double angle, ax, ay, az;
             scene >> angle >> ax >> ay >> az;
@@ -351,16 +399,16 @@ int main(void)
 
         else if (cmd == "push")
         {
-            cout << "push"<< endl;
+            //cout << "push"<< endl;
             sm.push(currentTop);
             //currentTop = Matrix::identityMatrix(4);
         }
 
         else if (cmd == "pop")
         {
+            //cout << "pop"<< endl;
             currentTop = sm.top();
             sm.pop();
-            cout << "pop"<< endl;
         }
 
         else if (cmd == "end")
@@ -411,8 +459,7 @@ int main(void)
     ofstream stage2;
     stageIn.open("stage1.txt");
     stage2.open("stage2.txt");
-    stage2 << std::fixed;
-    stage2 << std::setprecision(7);
+    stage2 << std::fixed << std::setprecision(7);
 
     int tCountTemp = tCount;
     while( tCountTemp )
@@ -449,13 +496,11 @@ int main(void)
     P.matrix[3][2] = -1;
     P.matrix[3][3] = 0;
 
-    P.print();
     ifstream stageIn2;
     ofstream stage3;
     stageIn2.open("stage2.txt");
     stage3.open("stage3.txt");
-    stage3 << std::fixed;
-    stage3 << std::setprecision(7);
+    stage3 << std::fixed << std::setprecision(7);
 
     tCountTemp = tCount;
     while( tCountTemp )
@@ -480,6 +525,8 @@ int main(void)
     stage3.close();
 
 
+
+
     // stage 4 : Clipping & scan conversion using Z-buffer algorithm
 
     ifstream stageIn3;
@@ -488,8 +535,9 @@ int main(void)
     stageIn3.open("stage3.txt");
     config.open("config.txt");
     z_buffer.open("z_buffer.txt");
+    z_buffer << std::fixed << std::setprecision(6);
 
-    double screenWidth, screenHeight;
+    int screenWidth, screenHeight;
     double leftLimit, rightLimit, bottomLimit, topLimit, frontLimit, rearLimit;
 
     config >> screenWidth >> screenHeight;
@@ -499,13 +547,226 @@ int main(void)
     config >> frontLimit >> rearLimit;
     config.close();
 
+    Triangle triangles[tCount];
+
+    //storing the file inputs into Triangle data structure
+    tCountTemp = tCount;
+    while (tCountTemp)
+    {
+        Point p[3];
+        for(int i = 0; i < 3; i++)
+        {
+            stageIn3 >> p[i].x >> p[i].y >> p[i].z;
+            p[i].w = 1;
+        }
+
+        Color c(rand()%255, rand()%255, rand()%255);
+        Triangle temp(p[0], p[1], p[2], c);
+        triangles[tCount-tCountTemp] = temp;
+
+        tCountTemp--;
+    }
+
+
+    //initializing necessary variables
+    double dx = (rightLimit - leftLimit)/screenWidth;
+    double dy = (topLimit - bottomLimit)/screenHeight;
+
+    double topY = topLimit - dy/2;
+    double leftX = leftLimit + dx/2;
+    double bottomY = bottomLimit + dy/2;
+    double rightX = rightLimit - dx/2;
+
+
+    //initializing z_buffer array
+    double** zBufferArray = new double*[screenHeight];
+    for (int i = 0; i < screenWidth; i++)
+        zBufferArray[i] = new double[screenWidth];
+
+    for (int i = 0; i < screenHeight; i++)
+    {
+        for (int j = 0; j < screenWidth; j++)
+            zBufferArray[i][j] = rearLimit;
+    }
+
+
+    //initializing image buffer and setting background as black.
+    bitmap_image image(screenWidth, screenHeight);
+    for(int i = 0; i < screenWidth; i++)
+    {
+        for (int j = 0; j < screenHeight; j++)
+            image.set_pixel(i,j,0, 0, 0);
+    }
 
 
 
+    //Start of Main Algorithm
+
+    //for every triangle
+    for (int i = 0; i < tCount; i++)
+    {
+        Triangle triangle = triangles[i];
+        double a = triangle.points[0].y;
+        double b = triangle.points[1].y;
+        double c = triangle.points[2].y;
+
+        // maxX, maxY, minX, minY refers max and min value of the triangle
+        double maxY=( a>=b&&a>=c)?a:(b>=a&&b>=c)?b:c;
+        double minY=(a<=b&&a<=c)?a:(b<=a&&b<=c)?b:c;
+
+        // if max, min values go outside of the viewing area, clipped them.
+        if (maxY > topY)
+            maxY = topY;
+        if (minY < bottomY)
+            minY = bottomY;
+
+
+        a = triangle.points[0].x;
+        b = triangle.points[1].x;
+        c = triangle.points[2].x;
+
+        double maxX=(a>=b&&a>=c)?a:(b>=a&&b>=c)?b:c;
+        double minX=(a<=b&&a<=c)?a:(b<=a&&b<=c)?b:c;
+
+        if (maxX > rightX)
+            maxX = rightX;
+        if (minX < leftX)
+            minX = leftX;
+
+
+        //calculate top scanLine and bottom scanLine
+        int topRow = round((topY - maxY)/dy);
+        int bottomRow = round((topY - minY)/dy);
+
+        //for each scanLine
+        for (int row = topRow; row < bottomRow; row++)
+        {
+            // ys--> y co-ordinate of a specific row
+            double ys = topY - row * dy;
+
+            //for finding left starting point and right starting point of a scanLine
+            double xa = 1000, xb = 1000, xc = 1000, za = 1000, zb = 1000, zc = 1000;
+
+            //calculating 3 possible intersecting point.
+            if ( (triangle.points[1].y - triangle.points[0].y) != 0 )
+            {
+                xa = triangle.points[0].x + (ys - triangle.points[0].y)*(triangle.points[1].x - triangle.points[0].x)/(triangle.points[1].y - triangle.points[0].y);
+               // za = triangle.points[0].z + (ys - triangle.points[0].y)*(triangle.points[1].z - triangle.points[0].z)/(triangle.points[1].y - triangle.points[0].y);
+            }
+
+            if ( (triangle.points[2].y - triangle.points[0].y) != 0 )
+            {
+                xb = triangle.points[0].x + (ys - triangle.points[0].y)*(triangle.points[2].x - triangle.points[0].x)/(triangle.points[2].y - triangle.points[0].y);
+               // zb = triangle.points[0].z + (ys - triangle.points[0].y)*(triangle.points[2].z - triangle.points[0].z)/(triangle.points[2].y - triangle.points[0].y);
+            }
+
+            if ( (triangle.points[2].y - triangle.points[1].y) != 0 )
+            {
+                xc = triangle.points[1].x + (ys - triangle.points[1].y)*(triangle.points[2].x - triangle.points[1].x)/(triangle.points[2].y - triangle.points[1].y);
+               // zc = triangle.points[1].z + (ys - triangle.points[1].y)*(triangle.points[2].z - triangle.points[1].z)/(triangle.points[2].y - triangle.points[1].y);
+            }
+
+            // for leftX and rightX value of a row(scanLine)
+            double maxa;
+            double mina;
+
+            if ( xa == 1000 )
+            {
+                maxa = (xb >= xc)?xb:xc;
+                mina = (xb <= xc)?xb:xc;
+            }
+            else if ( xb == 1000 )
+            {
+                maxa = (xa >= xc)?xa:xc;
+                mina = (xa <= xc)?xa:xc;
+            }
+            else if ( xc == 1000 )
+            {
+                maxa = (xa >= xb)?xa:xb;
+                mina = (xa <= xb)?xa:xb;
+            }
+            else if ( (xa == 1000 && xb == 1000) || (xc == 1000 && xb == 1000) || (xa == 1000 && xc == 1000) )
+            {
+                cout << "continue" << endl;
+                continue;
+            }
+            else
+            {
+                if ( xa > maxX || xa < minX )
+                {
+                    maxa = (xb >= xc)?xb:xc;
+                    mina = (xb < xc)?xb:xc;
+                }
+                else if ( xb > maxX || xb < minX  )
+                {
+                    maxa = (xa >= xc)?xa:xc;
+                    mina = (xa < xc)?xa:xc;
+                }
+                else
+                {
+                    maxa = (xb >= xa)?xb:xa;
+                    mina = (xb < xa)?xb:xa;
+                }
+            }
+
+
+            //calculating z values for the two intersecting points
+            //za --> z value of left intersection point. zb --> right intersection point.
+            if ( maxa == xa)
+                zb = triangle.points[0].z + (ys - triangle.points[0].y)*(triangle.points[1].z - triangle.points[0].z)/(triangle.points[1].y - triangle.points[0].y);
+            else if (maxa == xb)
+                zb = triangle.points[0].z + (ys - triangle.points[0].y)*(triangle.points[2].z - triangle.points[0].z)/(triangle.points[2].y - triangle.points[0].y);
+            else
+                zb = triangle.points[1].z + (ys - triangle.points[1].y)*(triangle.points[2].z - triangle.points[1].z)/(triangle.points[2].y - triangle.points[1].y);
+
+
+            if ( mina == xa)
+                za = triangle.points[0].z + (ys - triangle.points[0].y)*(triangle.points[1].z - triangle.points[0].z)/(triangle.points[1].y - triangle.points[0].y);
+            else if (mina == xb)
+                za = triangle.points[0].z + (ys - triangle.points[0].y)*(triangle.points[2].z - triangle.points[0].z)/(triangle.points[2].y - triangle.points[0].y);
+            else
+                za = triangle.points[1].z + (ys - triangle.points[1].y)*(triangle.points[2].z - triangle.points[1].z)/(triangle.points[2].y - triangle.points[1].y);
+
+
+            // if max, min values go outside of the viewing area, clipped them.
+            if (maxa > rightX)
+                maxa = rightX;
+            if (mina < leftX)
+                mina = leftX;
+
+
+            //calculating left and right column number.
+            int leftCol = round((mina - leftX)/dx);
+            int rightCol = round((maxa - leftX)/dx);
+
+            //for each column(in which we need to set pixel value) in a scanLine.
+            for (int col = leftCol; col < rightCol; col++)
+            {
+                double xp = leftX + col * dx;
+                //updating z value in a corresponding pixel
+                double zp = za + (xp - mina) * (zb - za)/(maxa - mina);
+
+                if ( zp > frontLimit && zp < zBufferArray[row][col] )
+                {
+                    zBufferArray[row][col] = zp;
+                    image.set_pixel(col, row, triangle.color.R, triangle.color.G, triangle.color.B);
+
+                    z_buffer << zp << "\t" ;
+                }
+            }
+            z_buffer << endl;
+        }
+    }
+
+
+    image.save_image("output.bmp");
 
     stageIn3.close();
     z_buffer.close();
 
+    //free the z_buffer array
+    for (int i = 0; i < screenWidth; i++)
+        delete[] zBufferArray[i];
+
     return 0;
 }
-
